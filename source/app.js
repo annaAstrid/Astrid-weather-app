8b72edf249110a11bfa72e64f899d817f2c66258
annaAstrid <anna.laszczynska8@gmail.com>
2022-03-05 11:15:11 +0100

    fixed api key

--- source/app.js
+++ source/app.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "57f82f70597c837a16ceb1";
+  let apiKey = "1c0c44dc4657f82f70597c837a16ceb1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
